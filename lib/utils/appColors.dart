261dd6317d16ecba71ca6f0c5b6ca3be3d9ccaad
Rubas <rubasakhter.11@gmail.com>
2024-07-24 01:32:07 +0500

    fonts added and code refactored

---
+++ lib/utils/appColors.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+class AppColors{
+  static const whiteColor=Color.fromRGBO(224, 223, 225,0.92);
+  static const blackColor=Color.fromRGBO(37, 36, 37,100);
+}
